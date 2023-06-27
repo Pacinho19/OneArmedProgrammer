d18b36c6436ddefc7df06ca795f51b089dcfd680
Pacinho19 <ojdana.patryk.java@gmail.com>
2023-06-27 15:03:28 +0200

    Home page with simple board view prototype

--- src/main/java/pl/pacinho/onearmedprogrammer/config/GameConfig.java
+++ src/main/java/pl/pacinho/onearmedprogrammer/config/GameConfig.java
@@ -1,4 +1,8 @@
 package pl.pacinho.onearmedprogrammer.config;
 
 public class GameConfig {
+
+    public static final int MIN_POWER_FACTOR = 1;
+    public static final int MAX_POWER_FACTOR = 100;
+    public static final int POWER_FACTOR_STEP = 1;
 }
