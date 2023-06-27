a71090109cb3a2a80d0c493a1cc36ab6169ac4ae
Pacinho19 <ojdana.patryk.java@gmail.com>
2023-06-27 14:27:41 +0200

    Initial commit

---
+++ src/main/java/pl/pacinho/onearmedprogrammer/OneArmedProgrammerApplication.java
@@ -0,0 +1,13 @@
+package pl.pacinho.onearmedprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneArmedProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneArmedProgrammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pacinho/onearmedprogrammer/OneArmedProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pacinho.onearmedprogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneArmedProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
